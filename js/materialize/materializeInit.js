fb446e8b9a304459aafa2e46e34dd59e5ac5bdf4
Zombitch <ashbay.interactive@gmail.com>
2018-03-05 22:50:14 +0100

    Clean up

--- js/materialize/materializeInit.js
+++ js/materialize/materializeInit.js
@@ -4,6 +4,7 @@
 function MaterializeCustomInit(){
   $('.modal').modal();
   $(".side-button").sideNav();
+
   $('select').material_select();
   $("#canVectMenu").css("max-height", $("#canVect").css("height"));
   $("#canVectMenu").css("overflow", "auto");
