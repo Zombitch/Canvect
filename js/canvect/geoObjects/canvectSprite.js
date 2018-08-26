4e88a59a5008c1061b8bae1dc5ca54efddc96417
Zombitch <ashbay.interactive@gmail.com>
2018-08-26 02:10:32 +0200

    Improving sprite animation

--- js/canvect/geoObjects/canvectSprite.js
+++ js/canvect/geoObjects/canvectSprite.js
@@ -2,9 +2,12 @@ function CVSprite(){
   var self = this;
   this.lineSize = null;
   this.columnSize = null;
+  this.currentFrame = null;
+  this.frameSpeed = null;
+  this.direction = null;
+  this.animate = null;
 
-
-  CVPoint.call(this);
+  CVImage.call(this);
 
   /**
   *  Default initializer that instanciate the attributes
@@ -13,6 +16,10 @@ function CVSprite(){
     this.baseInit();
     this.type = CVObjectType.IMAGE();
     this.image = new Image();
+    this.currentFrame = 0;
+    this.direction = 0;
+    this.frameSpeed = 1;
+    this.animate = false;
   }
 
   /**
@@ -23,10 +30,46 @@ function CVSprite(){
   }
 
   /**
+  * Set sprite direction (in fact it means select the line we will display)
+  */
+  this.setLineSize = function(size){
+    this.lineSize = size;
+  }
+
+  /**
+  * Set if the sprite should animate or not
+  */
+  this.setAnimate = function(shouldAnimate){
+    this.animate = shouldAnimate;
+  }
+
+  /**
+  * Set current frame
+  */
+  this.setCurrentFrame = function(frame){
+    this.currentFrame = frame;
+  }
+
+  /**
+  * Set frame speed
+  */
+  this.setFrameSpeed = function(frameSpeed){
+    this.frameSpeed = frameSpeed;
+  }
+
+  /**
   * Set colomn size
   */
   this.setColumnSize = function(size){
     this.columnSize = size;
+  }
+
+  /**
+  * Stop animating the sprite and display given frame
+  */
+  this.stopAtFrame = function (frameNumber){
+    this.animate = false;
+    this.currentFrame = frame;
   }
 
   /**
@@ -43,7 +86,26 @@ function CVSprite(){
   */
   this.draw = function(ctx){
     if(this.isImageLoaded == true){
-      ctx.drawImage(this.image, this.x, this.y);
+      var spriteElementWidth = (this.image.width / this.columnSize);
+      var spriteElementHeight = (this.image.height / this.lineSize);
+      ctx.drawImage(this.image, (this.currentFrame*spriteElementWidth), (this.direction*spriteElementHeight), spriteElementWidth, spriteElementHeight, this.x, this.y, spriteElementWidth, spriteElementHeight);
+
+      if(this.animate){
+        this.currentFrame += this.frameSpeed;
+        if(this.currentFrame >= this.columnSize) this.currentFrame = 0;
+      }
+    }
+  }
+
+  /**
+  * Draw a part of CVImage on the canvas.
+  * @param ctx Canvas context
+  */
+  this.drawCell = function(ctx, columnNumber, lineNumber){
+    if(this.isImageLoaded == true){
+      var spriteElementWidth = (this.image.width / this.columnSize);
+      var spriteElementHeight = (this.image.height / this.lineSize);
+      ctx.drawImage(this.image, columnNumber*spriteElementWidth, lineNumber*spriteElementHeight, spriteElementWidth, spriteElementHeight, this.x, this.y, spriteElementWidth, spriteElementHeight);
     }
   }
 }
